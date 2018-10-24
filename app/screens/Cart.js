969bb0f902264c421dab9470f2b287a34564c480
Uxman <ZAKI_US94@HOTMAIL.COM>
2018-10-24 18:33:57 +0500

    added CartScreen

--- app/screens/Cart.js
+++ app/screens/Cart.js
@@ -8,7 +8,10 @@ export default class Cart extends Component {
     constructor(){
         super()
         this.state = {
-            isChat: false
+            isChat: false,
+            number1: 0,
+            number2: 0,
+            number3: 0
         }
     }
     
@@ -19,6 +22,24 @@ export default class Cart extends Component {
         else
             this.setState({isChat: false})
     }
+
+    addNumber = (num) => {
+        if(num === this.state.number1)
+            this.setState({number1: ++num})
+        else if(num === this.state.number2)
+            this.setState({number2: ++num})
+        else
+            this.setState({number3: ++num})
+    }
+
+    subNumber= (num) => {
+        if(num === this.state.number1)
+            this.setState({number1: --num})
+        else if(num === this.state.number2)
+            this.setState({number2: --num})
+        else
+            this.setState({number3: --num})
+    }   
 
     render() {
         return (
@@ -51,9 +72,10 @@ export default class Cart extends Component {
                                 }}
                                 source={{ uri: 'https://www.dike.lib.ia.us/images/sample-1.jpg/image' }}
                             />
-                            <View style={{position: 'absolute', marginTop: 25, borderRadius: 5,
-                            marginLeft: 60, borderWidth: 1, backgroundColor: Colors.Green}}>
-                                <Text><Icon name="questioncircle" size={12}/> s51.89 </Text>
+                            <View style={{position: 'absolute', marginTop: 25, borderRadius: 5, flexDirection: 'row',
+                            marginLeft: 45,  backgroundColor: Colors.Green,padding:1}}>
+                                <Image source={require('../../assets/icons/question.png')} style={{width: 20, height: 20}}/>
+                                <Text style={{color: Colors.White}}> s51.89 </Text>
                             </View>
                             <View>
                                 <View style={{ flexDirection: 'row' }} >
@@ -68,27 +90,25 @@ export default class Cart extends Component {
                             </View>
                         </View>
                         <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 10 }}>
-                            <TouchableOpacity 
+                            <TouchableOpacity onPress={() => this.subNumber(this.state.number1)}
                             style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
-                                <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'blue', fontSize: 22 }}> - </Text>
+                                <Text style={styles.subtractBtnText} > - </Text>
                             </TouchableOpacity >
-                            <Text
-                                style={{
-                                    height: 30, borderRadius: 50, backgroundColor: 'white', width: 60,
-                                    textAlign: 'center', fontWeight: 'bold', fontSize: 18
-                                }}>
-                                02
-                         </Text>
-                            <TouchableOpacity 
+                            <Text style={styles.showText}>
+                                {this.state.number1}
+                            </Text>
+                            <TouchableOpacity onPress={() => this.addNumber(this.state.number1)}
                             style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
-                                <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'blue', fontSize: 22 }}> + </Text>
+                                <Text style={styles.addBtnText}> + </Text>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={this.openChat}
-                            style={{ height: 30, borderRadius: 5, backgroundColor: 'cyan', width: 90 }}>
-                                <Text><Icon name="chat-bubble-outline" size={20} color={'white'} /> Chat Now</Text>
+                            style={styles.chatBtn}>
+                                <Icon name="chat-bubble-outline" size={20} color={'white'} />
+                                <Text style={{color: Colors.White}}> Chat Now</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={{ height: 30, borderRadius: 5, backgroundColor: '#ffc200', width: 90 }}>
-                                <Text> <Icon name="chat-bubble-outline" size={20} /> s$1244.2</Text>
+                            <TouchableOpacity style={styles.tagBtn}>
+                                <Image source={require('../../assets/icons/loyalitybadge.png')} style={{width: 20, height: 20}}/>
+                                <Text style={{color: Colors.White}}>  s$1244.2</Text>
                             </TouchableOpacity>
                         </View>
                         {this.state.isChat ?
@@ -111,9 +131,10 @@ export default class Cart extends Component {
                                 }}
                                 source={{ uri: 'https://www.dike.lib.ia.us/images/sample-1.jpg/image' }}
                             />
-                            <View style={{position: 'absolute', marginTop: 25, borderRadius: 5,
-                            marginLeft: 60, borderWidth: 1, backgroundColor: Colors.Green}}>
-                                <Text><Icon name="questioncircle" size={12}/> s51.89 </Text>
+                            <View style={{position: 'absolute', marginTop: 25, borderRadius: 5, flexDirection: 'row',
+                            marginLeft: 45, borderWidth: 1, backgroundColor: Colors.Green}}>
+                                <Image source={require('../../assets/icons/question.png')} style={{width: 20, height: 20}}/>
+                                <Text style={{color: Colors.White}}> s51.89 </Text>
                             </View>
                             <View >
                                 <View style={{flexDirection: 'row' }} >
@@ -128,27 +149,23 @@ export default class Cart extends Component {
                             </View>
                         </View>
                         <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 10 }}>
-                            <TouchableOpacity style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
-                                <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'blue', fontSize: 22 }}> - </Text>
+                            <TouchableOpacity onPress={() => this.subNumber(this.state.number2)}
+                            style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
+                                <Text style={styles.subtractBtnText}> - </Text>
                             </TouchableOpacity >
-                            <Text
-                                style={{
-                                    height: 30, borderRadius: 50, backgroundColor: 'white', width: 60,
-                                    textAlign: 'center', fontWeight: 'bold', fontSize: 18
-                                }}>
-                                02
-                         </Text>
-                            <TouchableOpacity 
+                            <Text style={styles.showText}> {this.state.number2} </Text>
+                            <TouchableOpacity onPress={() => this.addNumber(this.state.number2)}
                                 style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
-                                <Text 
-                                style={{ textAlign: 'center', fontWeight: 'bold', color: 'blue', fontSize: 22 }}> + </Text>
+                                <Text style={styles.addBtnText}> + </Text>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={this.openChat}
-                            style={{ height: 30, borderRadius: 5, backgroundColor: 'cyan', width: 90 }}>
-                                <Text><Icon name="chat-bubble-outline" size={20} color={'white'} /> Chat Now</Text>
+                            style={styles.chatBtn}>
+                                <Icon name="chat-bubble-outline" size={20} color={'white'} />
+                                <Text style={{color: Colors.White}}> Chat Now</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={{ height: 30, borderRadius: 5, backgroundColor: '#ffc200', width: 90 }}>
-                                <Text> <Icon name="chat-bubble-outline" size={20} /> s$1244.2</Text>
+                            <TouchableOpacity style={styles.tagBtn}>
+                                <Image source={require('../../assets/icons/loyalitybadge.png')} style={{width: 20, height: 20}}/>
+                                <Text style={{color: Colors.White}}>  s$1244.2</Text>
                             </TouchableOpacity>
                         </View>
                         {this.state.isChat ?
@@ -170,13 +187,15 @@ export default class Cart extends Component {
                                 }}
                                 source={{ uri: 'https://www.dike.lib.ia.us/images/sample-1.jpg/image' }}
                             />
-                            <View style={{position: 'absolute', marginTop: 25, borderRadius: 5,
-                            marginLeft: 60, borderWidth: 1, backgroundColor: Colors.Green}}>
-                                <Text style={{textAlign: 'center'}}><Icon name="questioncircle" size={12}/> s51.89 </Text>
+                            <View style={{position: 'absolute', marginTop: 25, borderRadius: 5, flexDirection: 'row',
+                            marginLeft: 45, borderWidth: 1, backgroundColor: Colors.Green}}>
+                                <Image source={require('../../assets/icons/question.png')} style={{width: 20, height: 20}}/>
+                                <Text style={{color: Colors.White}}> s51.89 </Text>
                             </View>
                             <View>
                                 <View style={{ flexDirection: 'row' }} >
-                                    <Text style={{ fontWeight: 'bold', fontSize: 18, paddingLeft: 5 }}>Kare Tepsi Buyuk          </Text>
+                                    <Text 
+                                    style={{ fontWeight: 'bold', fontSize: 18, paddingLeft: 5 }}>Kare Tepsi Buyuk          </Text>
                                     <Icon name="message" size={20} />
                                 </View>
                                 <Text style={{ width: '100%', paddingLeft: 5 }} >12 pieces per Box (Package Box)</Text>
@@ -187,28 +206,27 @@ export default class Cart extends Component {
                             </View>
                         </View>
                         <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 10 }}>
-                            <TouchableOpacity style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
-                                <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'blue', fontSize: 22 }}> - </Text>
+                            <TouchableOpacity onPress={() => this.subNumber(this.state.number3)}
+                            style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
+                                <Text style={styles.subtractBtnText}> - </Text>
                             </TouchableOpacity >
-                            <Text
-                                style={{
-                                    height: 30, borderRadius: 50, backgroundColor: 'white', width: 60,
-                                    textAlign: 'center', fontWeight: 'bold', fontSize: 18
-                                }}>
-                                02
-                         </Text>
-                            <TouchableOpacity style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
-                                <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'blue', fontSize: 22 }}> + </Text>
+                            <Text style={styles.showText}>
+                                {this.state.number3}
+                            </Text>
+                            <TouchableOpacity onPress={() => this.addNumber(this.state.number3)}
+                            style={{ height: 30, borderRadius: 50, backgroundColor: 'white', width: 30 }}>
+                                <Text style={styles.addBtnText}> + </Text>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={this.openChat} 
-                            style={{ height: 30, borderRadius: 5, backgroundColor: 'cyan', width: 90 }}>
-                                <Text>
-                                    <Icon name="chat-bubble-outline" size={20} color={'white'}/> 
+                            style={styles.chatBtn}>
+                                <Icon name="chat-bubble-outline" size={20} color={'white'}/>
+                                <Text style={{color: Colors.White}}>
                                         Chat Now
                                 </Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={{ height: 30, borderRadius: 5, backgroundColor: '#ffc200', width: 90 }}>
-                                <Text> <Icon name="chat-bubble-outline" size={20} style={styles.tagBtnIcon}/> s$1244.2</Text>
+                            <TouchableOpacity style={styles.tagBtn}>
+                                <Image source={require('../../assets/icons/loyalitybadge.png')} style={{width: 20, height: 20}}/>
+                                <Text style={{color: Colors.White}}>  s$1244.2</Text>
                             </TouchableOpacity>
                         </View>
                         {this.state.isChat ?
@@ -229,11 +247,11 @@ export default class Cart extends Component {
 
 const styles = StyleSheet.create({
     SubTotalAmount: {
-        color: 'black',
+        color: Colors.Grey,
         fontWeight: 'bold'
     },
     cbmText: {
-        color: 'cyan',
+        color: Colors.Green,
         fontWeight: 'bold'
     },
     buttonView: {
@@ -252,13 +270,52 @@ const styles = StyleSheet.create({
     },
     btnText: {
         textAlign: 'center',
-        color: Colors.White,
         fontWeight: 'bold',
         fontSize: 18,
+        color: Colors.White,
     },
     imageView: {
         flexDirection: 'row',
         paddingTop: 20,
         justifyContent: 'space-between',
     },
+    subtractBtnText: { 
+        textAlign: 'center', 
+        fontWeight: 'bold', 
+        color: 'blue', 
+        fontSize: 22 
+    },
+    showText: {
+        height: 30, 
+        borderRadius: 50, 
+        backgroundColor: 'white', 
+        width: 60,
+        textAlign: 'center', 
+        fontWeight: 'bold', 
+        fontSize: 18,
+    },
+    addBtnText: { 
+        textAlign: 'center', 
+        fontWeight: 'bold', 
+        color: 'blue', 
+        fontSize: 22 
+    },
+    chatBtn: { 
+        flexDirection: 'row',
+        height: 30, 
+        borderRadius: 5, 
+        backgroundColor: Colors.Green, 
+        width: 90, 
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tagBtn: {
+        flexDirection: 'row', 
+        height: 30, 
+        borderRadius: 5, 
+        backgroundColor: Colors.Yellow, 
+        width: 90,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 })
